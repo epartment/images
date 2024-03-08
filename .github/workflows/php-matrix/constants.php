0d5264048a7f43fbf877758fa99bbc4d666e5d0c
Rick <rickb1998@hotmail.com>
2024-03-08 15:09:05 +0100

    Added php 8.3 and node versions 20 & 21

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -1,7 +1,7 @@
 <?php
 
-const PHP_LATEST = '8.2';
-const PHP_VERSIONS = ['7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'];
+const PHP_LATEST = '8.3';
+const PHP_VERSIONS = ['7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3'];
 CONST PHP_VERSIONS_OS_RELEASE = [
     '7.0' => 'stretch',
     '7.1' => 'buster',
@@ -10,10 +10,11 @@ CONST PHP_VERSIONS_OS_RELEASE = [
     '7.4' => 'bullseye',
     '8.0' => 'bullseye',
     '8.1' => 'bullseye',
-    '8.2' => 'bullseye'
+    '8.2' => 'bullseye',
+    '8.3' => 'bullseye'
 ];
 const NODE_LATEST = '19';
-const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19'];
+const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'];
 CONST NODE_VERSIONS_OS_RELEASE = [
     '10' => 'stretch',
     '12' => 'bullseye',
@@ -23,7 +24,9 @@ CONST NODE_VERSIONS_OS_RELEASE = [
     '16' => 'bullseye',
     '17' => 'bullseye',
     '18' => 'bullseye',
-    '19' => 'bullseye'
+    '19' => 'bullseye',
+    '20' => 'bullseye',
+    '21' => 'bullseye'
 ];
 const EXPERIMENTAL_PHP_VERSIONS = [];
 const NOT_STABLE_XDEBUG_PHP_VERSIONS = ['7.0', '7.1', '7.2', '7.3', '7.4'];
