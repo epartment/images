7c6dcb6414f0113765bc939a2f7b8bcc3caa470d
Henk-Roel van Zijl <h.vanzijl@epartment.nl>
2025-02-26 10:19:56 +0100

    Crippled the matrix, so that I can test more quickly

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -1,33 +1,35 @@
 <?php
 
 const PHP_LATEST = '8.3';
-const PHP_VERSIONS = ['7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3'];
+// const PHP_VERSIONS = ['7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3'];
+const PHP_VERSIONS = ['8.3'];
 CONST PHP_VERSIONS_OS_RELEASE = [
-    '7.2' => 'buster',
-    '7.3' => 'bullseye',
-    '7.4' => 'bullseye',
-    '8.0' => 'bullseye',
-    '8.1' => 'bullseye',
-    '8.2' => 'bullseye',
+    // '7.2' => 'buster',
+    // '7.3' => 'bullseye',
+    // '7.4' => 'bullseye',
+    // '8.0' => 'bullseye',
+    // '8.1' => 'bullseye',
+    // '8.2' => 'bullseye',
     '8.3' => 'bullseye',
 ];
 const NODE_LATEST = '22';
-const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'];
+// const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'];
+const NODE_VERSIONS = ['22'];
 
 const EOL_NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '19'];
 
 const NODE_VERSIONS_OS_RELEASE = [
-    '10' => 'stretch',
-    '12' => 'bullseye',
-    '13' => 'buster',
-    '14' => 'bullseye',
-    '15' => 'buster',
-    '16' => 'bullseye',
-    '17' => 'bullseye',
-    '18' => 'bullseye',
-    '19' => 'bullseye',
-    '20' => 'bullseye',
-    '21' => 'bullseye',
+    // '10' => 'stretch',
+    // '12' => 'bullseye',
+    // '13' => 'buster',
+    // '14' => 'bullseye',
+    // '15' => 'buster',
+    // '16' => 'bullseye',
+    // '17' => 'bullseye',
+    // '18' => 'bullseye',
+    // '19' => 'bullseye',
+    // '20' => 'bullseye',
+    // '21' => 'bullseye',
     '22' => 'bullseye',
 ];
 const EXPERIMENTAL_PHP_VERSIONS = [];
