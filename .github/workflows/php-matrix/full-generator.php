fa7708738365e89d8e450ddd0e8acd93c0d840d4
Mike Laurense <m.laurense@epartment.nl>
2025-08-12 19:27:26 +0200

    Try a fix for xdebug matrix

--- .github/workflows/php-matrix/full-generator.php
+++ .github/workflows/php-matrix/full-generator.php
@@ -10,18 +10,18 @@ foreach (PHP_VERSIONS as $phpVersion) {
     $experimental = in_array($phpVersion, EXPERIMENTAL_PHP_VERSIONS);
     $endOfLife = in_array($phpVersion, EOL_PHP_VERSIONS);
     foreach (ARCHES as $arch) {
-        $matrix[] = [
-            'php_version' => $phpVersion,
-            'php_os_release' => $phpOsRelease,
-            'node_version' => 'x',
-            'node_os_release' => 'x',
-            'xdebug_type' => $xdebugType,
-            'experimental' => $experimental,
-            'end_of_life' => $endOfLife,
-            'continue_on_error' => $endOfLife || $experimental,
-            'latest' => $phpVersion === PHP_LATEST,
-            'arch' => $arch,
-        ];
+//        $matrix[] = [
+//            'php_version' => $phpVersion,
+//            'php_os_release' => $phpOsRelease,
+//            'node_version' => 'x',
+//            'node_os_release' => 'x',
+//            'xdebug_type' => $xdebugType,
+//            'experimental' => $experimental,
+//            'end_of_life' => $endOfLife,
+//            'continue_on_error' => $endOfLife || $experimental,
+//            'latest' => $phpVersion === PHP_LATEST,
+//            'arch' => $arch,
+//        ];
         foreach (NODE_VERSIONS as $nodeVersion) {
             $nodeOsRelease = array_key_exists($nodeVersion, NODE_VERSIONS_OS_RELEASE) ? NODE_VERSIONS_OS_RELEASE[$nodeVersion] : 'bullseye';
             $endOfLife = in_array($nodeVersion, EOL_NODE_VERSIONS);
